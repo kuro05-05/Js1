7f4f2231e56db487be47ac25e11635d1c229f4e8
kuro05-05 <kuroda.msk0924@gmail.com>
2023-12-04 23:18:16 +0900

    メモ

--- memo.rb
+++ memo.rb
@@ -1,38 +1,34 @@
+require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
 
-require "csv"
+puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 
-    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
-    memo_type = gets.to_i
-
- if memo_type == 1
-
-
-   puts "新規でメモを記載します。拡張子を除いたファイル名を入力して下さい。"
+if memo_type == 1
+   puts "拡張子を除いたファイルを入力してください"
    file_name = gets.chomp
-
-   puts "メモ内容を記載してください。"
-   puts "Ctrl+Dで保存します。"
+   
+   puts "メモ内容を記載してください"
+   puts "Ctrl+Dで保存します"
    memo = STDIN.read
 
    CSV.open("#{file_name}.cvs", "w") do |csv|
    csv << memo
+end
 
- end
+elsif
+   memo_type == 2
+   puts "既存のメモを編集します。各調子を除いた既存のファイル名を入力して下さい。"
+   file_name = gets.chomp
 
- elsif
-     memo_type == 2
+   puts "編集したい内容を入力して下さい。完了したらCtrl+Dを押して下さい"
+   memo = STDIN.read
 
-     puts "既存のメモを編集します。各調子を除いた既存のファイル名を入力して下さい。"
-     file_name = gets.chomp
+   CSV.open("#{file_name}.csv", "a") do |csv|
+   csv << memo
+end
 
-     puts "編集したい内容を入力して下さい。完了したらCtrl+Dを押して下さい"
-     memo = STDIN.read
-
-     CSV.open("#{file_name}.csv", "a") do |csv|
-     csv << memo
-  end
-
- else
+elsif
     puts "1か2を入力して下さい。"
- end
+end
+
